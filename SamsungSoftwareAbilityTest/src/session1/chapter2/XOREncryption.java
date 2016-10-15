7e428bbd06f4d3a42e9e945766dbc901fbe59f2a
imdasom <dasom7978@naver.com>
2016-10-15 15:25:21 +0900

    +삼성 소프트웨어 역량테스트, p50, XOR암호화, 메인코드

--- SamsungSoftwareAbilityTest/src/session1/chapter2/XOREncryption.java
+++ SamsungSoftwareAbilityTest/src/session1/chapter2/XOREncryption.java
@@ -1,13 +1,29 @@
 package session1.chapter2;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 /**
  * Created by dasom on 2016-10-15.
  */
 public class XOREncryption {
-    public static void main(String[] args){
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("SamsungSoftwareAbilityTest\\data\\XOREncryption.txt"));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("SamsungSoftwareAbilityTest\\data\\XOREncryptionInputData.txt"));
+        String key = bufferedReader.readLine();
+        String data = bufferedReader.readLine();
+        bufferedReader.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<key.length(); i++){
+            sb.append((char)(key.charAt(i)^data.charAt(i)));
+        }
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\dasom\\Desktop\\personal_algo_study\\workspace-intellij\\SamsungSoftwareAbilityTest\\data\\XOREncryptionOutputData.txt"));
+        bufferedWriter.write(sb.toString());
+        bufferedWriter.newLine();
+        bufferedWriter.write(data);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 }
